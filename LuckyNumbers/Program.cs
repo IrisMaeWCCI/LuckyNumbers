5726e6f780279007adcbd5c3462656f55dbc10ee
IrisMaeWCCI <1miss1hicks1@gmail.com>
2017-07-11 00:11:02 -0400

    parts 1, 2,3 added

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -15,58 +15,67 @@ namespace LuckyNumbers
 
             Console.WriteLine("And provide me with a ending number");
             int endNumber = int.Parse(Console.ReadLine());
+
             //User's Guess Numbers
-            Console.WriteLine(" Please provide me with your guess number 1");
-            int guessNumber1 = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine(" Please provide me with your guess number 2");
-            int guessNumber2 = int.Parse(Console.ReadLine());
+            //Create an array in which I know the length
 
-            Console.WriteLine(" Please provide me with your guess number 3");
-            int guessNumber3 = int.Parse(Console.ReadLine());
+            int[] GuessingNumberArray = new int[6];
+           
+           
+            //loop thru n input user guessed #s
 
-            Console.WriteLine(" Please provide me with your guess number 4");
-            int guessNumber4 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(" Please provide me with your guess number 5");
-            int guessNumber5 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(" Please provide me with your guess number 6");
-            int guessNumber6 = int.Parse(Console.ReadLine());
-
-            //Array 
-            int[] GuessingNumberArray = { guessNumber1, guessNumber2, guessNumber3, guessNumber4, guessNumber5, guessNumber6 };
-
-            int userEnters = int.Parse(Console.ReadLine());
-            // while (userEnters == 2|| userEnters == 7 || userEnters == 9|| userEnters == 11 || userEnters == 13 || userEnters == 17)
-            // Console.WriteLine("You've guessed a lucky number!");
-            Random random = new Random();
-             if (userEnters <= startNumber || endNumber >= userEnters)
+            //Console.WriteLine("Lucky Number: {0},\nLucky Number: {1},\nLucky Number: {2},\nLucky Number: {3},\nLucky Number: {4},\nLucky Number: {5},\nLucky Number: {6}", GuessingNumberArray[0], GuessingNumberArray[1], GuessingNumberArray[2], GuessingNumberArray[3], GuessingNumberArray[4], GuessingNumberArray[5], GuessingNumberArray[6]);
+            for (int i = 0; i <= 5; i++)
             {
-                do
+                Console.WriteLine("Enter a number please");
+                int userNumber = int.Parse(Console.ReadLine());
+                while (userNumber < startNumber || userNumber > endNumber)
                 {
-                     Console.WriteLine("Lucky Number: "+ GuessingNumberArray);
-                    //Let's pretend the code for the game is her
-
-                    Console.WriteLine("Great game!!");
-                    Console.WriteLine("Do you want to play again? YES/NO");
-                    userEnters = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter a valid number");
+                    userNumber = int.Parse(Console.ReadLine());
                 }
-                while (userEnters > startNumber || endNumber < userEnters);
-            }
-            int jackpot = 123456789;
-            Console.WriteLine("The jackpot amount is 123,456,789");
-            string tryAgain; 
-            do
-            {
-                Console.WriteLine("Welcome to my game!");
-                //Let's pretend the code for the game is here
-                Console.WriteLine("Great game!!");
-                Console.WriteLine("Do you want to play again? YES/NO");
-                tryAgain = Console.ReadLine();
-            }
-            while (tryAgain == "YES");
 
+                GuessingNumberArray[i] = userNumber;
+            }
+            Random random = new Random();
+            int[] LuckyNumbers = new int [6];
+            for (int i = 0; i<=5; i++)
+            {
+                LuckyNumbers[i] = random.Next(startNumber, endNumber+1);
+
+            }
+            foreach(int number in LuckyNumbers)
+            {
+                Console.WriteLine("Lucky Number : " + number);
+            }
+            int jackpot = 500;
+            Console.WriteLine("The jackpot is $" +jackpot+ "!");
+            int guessedCorrectly = 0;
+            for(int i = 0; i<5; i++)
+            {
+                for (int j=0; j<5; j++)
+                {
+                    if (LuckyNumbers[i] == GuessingNumberArray[j])
+                    {
+                        guessedCorrectly += 1;
+
+                    }
+                }
+            }
+            Console.WriteLine("You have guessed: "+guessedCorrectly+" correctly");
+
+            if (guessedCorrectly == 0)
+            {
+                Console.WriteLine("Sorry, you don't win any of the jackpot!");
+            }
+            else if (guessedCorrectly > 0 && guessedCorrectly <= 3)
+            {
+                Console.WriteLine("You will receive $" + jackpot/2);
+            }
+            else
+            {
+                Console.WriteLine("You will receive $"+ jackpot);
+            }
         }
     }
 }
